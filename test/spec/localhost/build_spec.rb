34cb6fa15f04acc995c886a02fa8276985fcca9a
Robert Northard <robertnorthard@googlemail.com>
2016-11-12 15:12:41 +0000

    Update tests to use alpine as os family.

--- test/spec/localhost/build_spec.rb
+++ test/spec/localhost/build_spec.rb
@@ -8,7 +8,7 @@ describe "Dockerfile#build" do
   before(:all) do
     @docker_image = Docker::Image.build_from_dir('.')
     set :backend, :docker
-    set :os, family: :ubuntu
+    set :os, family: :alpine:
     set :docker_image, @docker_image.id   
   end
 
